7129afc8cdbf5accae4d4a5bf42292e4354d282e
alexis nouvel <alexis.nouvel@neotys.com>
2017-12-20 18:05:20 +0100

    fix some sonar issues

--- src/main/java/com/neotys/dynatrace/events/DynatraceEventAPI.java
+++ src/main/java/com/neotys/dynatrace/events/DynatraceEventAPI.java
@@ -23,16 +23,6 @@ class DynatraceEventAPI {
 	private static final String START_NL_TEST = "Start NeoLoad Test";
 	private static final String STOP_NL_TEST = "Stop NeoLoad Test";
 
-	private static final int BAD_REQUEST = 400;
-	private static final int UNAUTHORIZED = 403;
-	private static final int NOT_FOUND = 404;
-	private static final int METHOD_NOT_ALLOWED = 405;
-	private static final int REQUEST_ENTITY_TOO_LARGE = 413;
-	private static final int INTERNAL_SERVER_ERROR = 500;
-	private static final int BAD_GATEWAY = 502;
-	private static final int SERVICE_UNAVAIBLE = 503;
-	private static final int GATEWAY_TIMEOUT = 504;
-
 	private final Map<String, String> headers;
 	private final String dynatraceApiKey;
 	private final String dynatraceAccountID;
