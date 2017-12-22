0162f59d5516aefbb34df57ed4db05e0d5a825ce
alexis nouvel <alexis.nouvel@neotys.com>
2017-12-22 09:33:39 +0100

    handle proxy in swagger http client

--- src/main/java/com/neotys/dynatrace/monitoring/DynatracePluginData.java
+++ src/main/java/com/neotys/dynatrace/monitoring/DynatracePluginData.java
@@ -1,10 +1,23 @@
 package com.neotys.dynatrace.monitoring;
 
 import com.google.common.base.Optional;
+import com.google.common.base.Strings;
+import com.neotys.dynatrace.common.DynatraceUtils;
 import com.neotys.extensions.action.engine.Context;
+import com.neotys.extensions.action.engine.Proxy;
+import com.squareup.okhttp.Authenticator;
+import com.squareup.okhttp.Credentials;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 import io.swagger.client.ApiClient;
 import io.swagger.client.api.ResultsApi;
 
+import javax.net.ssl.*;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.util.Timer;
 
 public class DynatracePluginData {
@@ -28,7 +41,7 @@ public class DynatracePluginData {
 
     public DynatracePluginData(final String dynataceApiKey, final String neoLoadWebApiKey,
                                final Optional<String> proxyName, final Context context, final String dynatraceId,
-                               final String dataExchangeApiUrl, final Optional<String> dynatraceManagedHostname) {
+                               final String dataExchangeApiUrl, final Optional<String> dynatraceManagedHostname) throws Exception {
 
         this.dynataceApiKey = dynataceApiKey;
         dynatraceAccountId = dynatraceId;
@@ -36,7 +49,12 @@ public class DynatracePluginData {
         //----define  the NLWEB API-----
         neoLoadWebApiClient = new ApiClient();
         neoLoadWebApiClient.setApiKey(neoLoadWebApiKey);
-        neoLoadWebApiClient.setBasePath(getBasePath(context));
+        final String basePath = getBasePath(context);
+        neoLoadWebApiClient.setBasePath(basePath);
+        final Optional<Proxy> proxyOptional = DynatraceUtils.getProxy(context, proxyName, basePath);
+        if(proxyOptional.isPresent()) {
+            initProxyForNeoloadWebApiClient(proxyOptional.get());
+        }
         this.dynatraceManagedHostname = dynatraceManagedHostname;
         initNeoLoadApi();
         //-------------------------
@@ -45,6 +63,65 @@ public class DynatracePluginData {
         this.dataExchangeApiUrl = dataExchangeApiUrl;
         neoLoadAggregator = new NeoLoadStatAggregator(dynataceApiKey, dynatraceAccountId, nlWebResult,
                 context, dataExchangeApiUrl, dynatraceManagedHostname, proxyName);
+    }
+
+    private void initProxyForNeoloadWebApiClient(final Proxy proxy) throws KeyManagementException, NoSuchAlgorithmException {
+        neoLoadWebApiClient.getHttpClient().setProxy(toOkHttpProxy(proxy));
+		if (!Strings.isNullOrEmpty(proxy.getLogin())) {
+			Authenticator proxyAuthenticator = new Authenticator() {
+				@Override
+				public Request authenticate(java.net.Proxy p, Response response) throws IOException {
+					final String credential = Credentials.basic(proxy.getLogin(), proxy.getPassword());
+					return response.request().newBuilder()
+							.header("Proxy-Authorization", credential)
+							.build();
+				}
+
+				@Override
+				public Request authenticateProxy(java.net.Proxy p, Response response) throws IOException {
+					final String credential = Credentials.basic(proxy.getLogin(), proxy.getPassword());
+					return response.request().newBuilder()
+							.header("Proxy-Authorization", credential)
+							.build();
+				}
+			};
+			neoLoadWebApiClient.getHttpClient().setAuthenticator(proxyAuthenticator);
+		}
+		// Create a trust manager that does not validate certificate chains
+		final TrustManager[] trustAllCerts = new TrustManager[]{
+				new X509TrustManager() {
+					@Override
+					public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+					}
+
+					@Override
+					public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+					}
+
+					@Override
+					public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+						return new java.security.cert.X509Certificate[0];
+					}
+				}
+		};
+
+		// Install the all-trusting trust manager
+		final SSLContext sslContext = SSLContext.getInstance("TLS");
+		sslContext.init(null, trustAllCerts, null);
+		// Create an ssl socket factory with our all-trusting manager
+		final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+		neoLoadWebApiClient.getHttpClient().setSslSocketFactory(sslSocketFactory);
+		neoLoadWebApiClient.getHttpClient().setHostnameVerifier(new HostnameVerifier() {
+			@Override
+			public boolean verify(String hostname, SSLSession session) {
+				return true;
+			}
+		});
+	}
+
+    private static java.net.Proxy toOkHttpProxy(final Proxy proxy) {
+        return new java.net.Proxy(java.net.Proxy.Type.HTTP, new InetSocketAddress(proxy.getHost(), proxy.getPort()));
     }
 
     private String getBasePath(final Context context) {
